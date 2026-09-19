bc33a08be6027bf4da050c6f9db7240ebfc18ac7
lxmonDxmon <dimitriy.zlatkin@gmail.com>
2026-09-19 15:45:11 -0500

    Created player object, made it move and jump

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [Header("Movement Controls")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rigidbody;
+    private bool isGrounded;
+
+    private void Awake() {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float moveInput = Input.GetAxis("Horizontal");
+        rigidbody.linearVelocity = new Vector2(moveInput * moveSpeed, rigidbody.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rigidbody.linearVelocity = new Vector2(rigidbody.linearVelocity.x, jumpForce);
+    }
+
+    private void FixedUpdate() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+}
